9df7431b68166ebfa87cb4b9b63e8bd288d53870
patrickmbell <patrickmbell@ufl.edu>
2019-04-05 12:33:29 -0400

    bash script included

--- hw10.java
+++ hw10.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 
 class PizzaPartyDriver {
     public static void main( String args[] ) {
-    OurPizzaParty opp = new OurPizzaParty();
-    opp.menu();
+        String passwd; 
+        String username = "root";
+        String db = "PizzaParty";
+        Scanner in = new Scanner(System.in);
+    
+        System.out.println("Password: "); 
+        passwd = in.nextLine(); 
+        OurPizzaParty opp = new OurPizzaParty(db, username, passwd);
+        opp.menu();
     }
 }
 
@@ -17,24 +24,27 @@ class OurPizzaParty {
     Statement statement; 
     String JDBC_DRIVER = "org.mysql.jdbc.Driver";
     String LOCAL_HOST = "jdbc:mariadb://localhost:3306/";
+    
     public OurPizzaParty() {
-    database = "PizzaParty";
-    username = "root";
-    password = "root";
+        database = "PizzaParty";
+        username = "root";
+        password = "root";
     }
     
     public OurPizzaParty( String database, String username, String password ) {
-    this.database = database;
-    this.username = username;
-    this.password = password;
+        this.database = database;
+        this.username = username;
+        this.password = password;
     
     try{
         connection = DriverManager.getConnection(LOCAL_HOST + this.database, this.username, this.password);
+        statement = connection.createStatement();
     }
     catch(SQLException e)
     {
         e.printStackTrace();
     }
+    
     }
 
     /*
@@ -43,14 +53,17 @@ class OurPizzaParty {
 
     public void menu() {
         String selection;
-        Scanner input = new Scanner();
+        Scanner in = new Scanner(System.in);
         while(true){
             System.out.println("(1) Execute a complete SQL command\n(2) Build an Order\n(3) Logout / End Program");
-            selection = input.nextLine();
+            selection = in.nextLine();
 
             if(selection.equals("1")){
-                selection = input.nextLine();
+                selection = in.nextLine();
                 executeQuery(selection); 
+            }
+            else if(selection.equals("3")){
+                System.exit(0); //0 indicates successful termination, 1 or -1 is unsuccessful termination.  
             }
         } 
 
@@ -58,10 +71,12 @@ class OurPizzaParty {
     }
 
     public void executeQuery(String query){
-        ResultSet rs; 
-        Connection connection; 
-        Statement statement; 
+        // rs = statement.executeQuery(query);
+        // while(rs.next()){
 
+        // }
+
+        return; 
     }
 
     // implement other methods as needed
